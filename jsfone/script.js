cd2a2a586dcfa658de7c9dd307b12a3e5adcba5f
Sumsuzzaman <joysho@gmail.com>
2021-06-10 01:49:15 +0600

    f01

---
+++ jsfone/script.js
@@ -0,0 +1,15 @@
+let js = "amazing";
+      if (js === "amazing") alert("JS is Fun!");
+
+      console.log(40 + 8 + 23 - 10);
+      let country = 'Bangladesh';
+      let continent = 'South Asia';
+      let population = "160";
+
+      console.log(country);
+      console.log(continent);
+      console.log(population);
+
+      let jsIsFun = true;
+      console.log(jsIsFun);
+      console.log(typeof jsIsFun);
